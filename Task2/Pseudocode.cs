107b913b4620711a457cd09d3841ac206b3af613
Jose Ruiloba <jose_ruiloba@outlook.com>
2019-01-20 23:17:07 -0700

    Final

---
+++ Task2/Pseudocode.cs
@@ -0,0 +1,46 @@
+//Create a program that gets the height and width of two rectangles from a user. Determine the area of the rectangles and then output 
+///which rectangle is larger or if they are equal.
+
+
+//setup
+single triangle1Height=0;
+single triangle1Width = 0;
+single triangle2Height = 0;
+single triangle2Width = 0;
+single triangle1Area = 0;
+single triangle1Area = 0;
+string Result=""
+bool isString = false = 0;
+
+//input
+write("Please enter the height of the first traingle");
+put triangle1Height
+
+write("Please enter the width of the first traingle");
+put triangle1Width
+
+write("Please enter the height of the second traingle");
+put triangle2Height
+
+write("Please enter the width of the second traingle");
+put triangle2Width
+
+
+//process
+triangle1Area=(1/2)* triangle1Width* triangle1Height;
+triangle2Area=(1/2)* triangle2Width* triangle2Height;
+
+if triangle1Area > triangle2Area 
+    "Triangle 1 is greater then triangle 2";
+else
+    "Triangle 2 is greater then triangle 1";
+
+// output
+
+write("The area for traingle 1 is ");
+out triangle1Area
+
+write("The area for traingle 1 is ");
+out triangle2Area
+
+    write (results)
--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -6,7 +6,97 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //setup
+            Single triangle1Height = 0;
+            Single triangle1Width = 0;
+            Single triangle2Height = 0;
+            Single triangle2Width = 0;
+            Single triangle1Area = 0;
+            Single triangle2Area = 0;
+            string inputValue = string.Empty;
+            string tryAgain = string.Empty;
+            string resultant = null;
+            bool isString = true;
+
+            //input
+            do
+            {
+                    do
+                    {
+                        Console.WriteLine("Please enter the height of the first triangle");
+                        inputValue = Console.ReadLine();
+                        isString = Single.TryParse(inputValue, out triangle1Height);
+
+                        if (!isString)
+                        {
+                            Console.WriteLine("You entered a string value, Please enter a valid number");
+                        }
+                    } while (!isString);
+
+                    do
+                    {
+                        Console.WriteLine("Please enter the width of the first triangle");
+                        inputValue = Console.ReadLine();
+                        isString = Single.TryParse(inputValue, out triangle1Width);
+
+                        if (!isString)
+                        {
+                            Console.WriteLine("You entered a string value, Please enter a valid number");
+                        }
+                    } while (!isString);
+                    do
+                    {
+                        Console.WriteLine("Please enter the height of the second triangle");
+                        inputValue = Console.ReadLine();
+                        isString = Single.TryParse(inputValue, out triangle2Height);
+
+                        if (!isString)
+                        {
+                            Console.WriteLine("You entered a string value, Please enter a valid number");
+                        }
+                    } while (!isString);
+
+                    do
+                    {
+                        Console.WriteLine("Please enter the width of the second triangle");
+                        inputValue = Console.ReadLine();
+                        isString = Single.TryParse(inputValue, out triangle2Width);
+
+                        if (!isString)
+                        {
+                            Console.WriteLine("You entered a string value, Please enter a valid number");
+                        }
+                    } while (!isString);
+
+                    //process
+                    triangle1Area = triangleArea(triangle1Height, triangle1Width);
+                    triangle2Area = triangleArea(triangle2Height, triangle2Width);
+
+
+                    //output
+                    if (triangle1Area > triangle2Area)
+                    {
+                        resultant = "The area of the first triangle " + triangle1Area + " is larger than the second triangle " + triangle2Area;
+                    }
+                    else if (triangle1Area < triangle2Area)
+                    {
+                        resultant = "The area of the second triangle " + triangle2Area + " is larger than the first triangle " + triangle1Area;
+                    }
+                    else
+                    {
+                        resultant = "Both triangles have equal area" + triangle1Area;
+                    }
+
+                    Console.WriteLine(resultant);
+                    Console.ReadLine();
+                    Console.WriteLine("Do you want to do this again? Y or N");
+                    tryAgain = Console.ReadLine().ToUpper();
+  
+            } while (tryAgain == "Y");
+        }
+        private static float triangleArea(float triangleHeight, float triangleWidth)
+        {
+            return .5f * triangleHeight * triangleWidth;
         }
     }
 }
